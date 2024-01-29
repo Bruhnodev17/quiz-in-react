485facbf0a13b5da7b97f26949667d7c7465cbf7
Bruhnodev17 <manoelbrunop17@gmail.com>
2024-01-29 11:26:56 -0300

    commit do projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Welcome from './components/Welcome'
+
+function App() {
+
+  return (
+    <>
+      <h1>Quiz de Programação</h1>
+      <Welcome />
+    </>
+  )
+}
+
+export default App
